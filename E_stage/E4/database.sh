b39dffff18ca2f241dc8a1ac2fa76cec2d802d2a
Rhye7 <caijingyun05@gmail.com>
2026-06-03 23:32:38 +0800

    E4 unfinished

---
+++ E_stage/E4/database.sh
@@ -0,0 +1,9 @@
+set -e
+./ex17 db.dat c
+./ex17 db.dat s 1 caijingyun caijingyun05@gmail.com
+./ex17 db.dat s 2 caijingyun 2023311B12@stu.hit.edu.cn
+./ex17 db.dat s 3 caijingyun caijingyun@ict.ac.cn
+./ex17 db.dat l
+rm db.dat
+
+
---
+++ E_stage/E4/ysyx_E4.md
@@ -0,0 +1,4 @@
+在看`https://wizardforcel.gitbooks.io/lcthw/content`的教程..又是线上c教程 
+6.2 看到第八章😶‍🌫️ 
+6.3 看到第三十一章😶‍🌫️ 
+然后我意识到要把所有附加题完成..
